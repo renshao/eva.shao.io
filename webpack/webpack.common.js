01ad5d150ce8684061b39bfbb58ffe765c24337b
Ren Shao <ren@shao.io>
2021-05-16 11:04:56 +1000

    Fix webpack build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -21,9 +21,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({ patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }] }),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html'),
-    }),
+    new HtmlWebpackPlugin(),
   ],
   resolve: {
     alias: {
@@ -39,12 +37,7 @@ module.exports = {
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-          },
-        },
+        type: 'asset/resource',
       },
     ],
   },
